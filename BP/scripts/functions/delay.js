35af48172fc2bea8a378033100bf7ef8c761766a
CibNumeritos <85264290+CibNumeritos@users.noreply.github.com>
2022-12-24 19:14:45 -0500

    Delete BP/scripts/functions directory

--- BP/scripts/functions/delay.js
+++
@@ -1,9 +0,0 @@
-import { system } from '@minecraft/server';
-export async function delay(ticks) {
-    return new Promise((resolve) => {
-        const sleep = system.runSchedule(() => {
-            resolve();
-            system.clearRunSchedule(sleep);
-        }, ticks);
-    });
-};
